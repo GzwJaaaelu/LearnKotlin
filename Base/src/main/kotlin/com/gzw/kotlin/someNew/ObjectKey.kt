84c227bba3d80caec758bb608b6fc48907d9dcd9
Gzw <gzw102030@outlook.com>
2017-09-13 21:50:17 +0800

    “静静态与单例

---
+++ Base/src/main/kotlin/com/gzw/kotlin/someNew/ObjectKey.kt
@@ -0,0 +1,23 @@
+package com.gzw.kotlin.someNew
+
+//  object 其实是一种特殊的 class
+//  所以它也可以实现接口或者集成类
+//  object 修饰后其实相当于 Java 中最简单的单例
+//  只有一个实例的类
+//  不能自定义构造方法
+
+object MusicPlay {
+    var state = 0
+
+    fun play(url: String) {
+
+    }
+
+    fun stop() {
+
+    }
+}
+
+fun main(args: Array<String>) {
+
+}
